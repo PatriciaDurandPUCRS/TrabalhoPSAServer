236d0ac40b68d6c8a89859abc48067bff0de00fa
patricia.durand <patricia.durand@agiplan.com>
2018-12-02 19:53:00 -0200

    Ajuste do login

--- src/main/java/br/com/trabalhoPSA/repository/AutenticacaoDAOImplement.java
+++ src/main/java/br/com/trabalhoPSA/repository/AutenticacaoDAOImplement.java
@@ -3,10 +3,12 @@ package br.com.trabalhoPSA.repository;
 import br.com.trabalhoPSA.entity.Credencial;
 import br.com.trabalhoPSA.entity.Usuario;
 import br.com.trabalhoPSA.mapper.UsuarioMapper;
+import br.com.trabalhoPSA.services.BaseService;
 import br.com.trabalhoPSA.services.HashingService;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -34,18 +36,18 @@ public class AutenticacaoDAOImplement implements AutenticacaoDAO {
     public ResponseEntity<Object> autenticar(Credencial credencial) {
         setDataSource();
         HttpStatus status = null;
-
-        System.out.println(credencial.toString());
+        HttpHeaders heders = BaseService.getHeders();
 
         try {
             String SQL = "SELECT * FROM USUARIO WHERE MATRICULA = ?";
             Usuario login = jdbcTemplateObject.queryForObject(SQL, new Object[]{credencial.getUsuario()}, new UsuarioMapper());
             status = (login.getSenha().equals(credencial.getSenha())) ? HttpStatus.OK : HttpStatus.BAD_REQUEST;
+            heders.set("role", login.getPermissao());
         } catch (Exception e) {
             log.error("Ocorreu um erro ao realizar o login.");
             log.error("[" + e.getLocalizedMessage() + "]");
         }
-        return new ResponseEntity<>(status);
+        return new ResponseEntity<>(heders, status);
     }
 
 //    @Override
--- src/main/java/br/com/trabalhoPSA/services/TokenService.java
+++ src/main/java/br/com/trabalhoPSA/services/TokenService.java
@@ -9,13 +9,13 @@ import java.util.Base64;
 @Component
 public class TokenService {
 
-    String originalInput = "test input";
-    String encodedString = Base64.getEncoder().encodeToString(originalInput.getBytes());
-
-    byte[] decodedBytes = Base64.getDecoder().decode(encodedString);
-    String decodedString = new String(decodedBytes);
-
-    JsonParser parser = new JsonParser();
-    JsonObject objJson = parser.parse(decodedString).getAsJsonObject();
+//    String originalInput = "test input";
+//    String encodedString = Base64.getEncoder().encodeToString(originalInput.getBytes());
+//
+//    byte[] decodedBytes = Base64.getDecoder().decode(encodedString);
+//    String decodedString = new String(decodedBytes);
+//
+//    JsonParser parser = new JsonParser();
+//    JsonObject objJson = parser.parse(decodedString).getAsJsonObject();
 
 }
